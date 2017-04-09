694b0573c90ebcb3458c88733a7454ebc5857f1e
Avinash Tiwari <avinashtiwari29@ymail.com>
2017-04-09 19:46:39 +0530

    Basic layout made

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(10);
+    }
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void buttonForThree(View view)
+    {
+        displayForTeamA(3);
+    }
+    public void buttonForTwo(View view)
+    {
+        displayForTeamA(2);
+    }
+    public void buttonForFreeThrow(View view)
+    {
+        displayForTeamA(1);
     }
 }
