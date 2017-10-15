cc97301fd793438ba6f4597a628c214bd30cd2f5
Avinash Tiwari <avinashtiwari29@ymail.com>
2017-10-15 19:09:02 +0530

    Final

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(scoreA);
+        displayForTeamB(scoreB);
     }
     int scoreA=0;
     int scoreB=0;
@@ -20,19 +21,47 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
-    public void buttonForThree(View view)
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void buttonForThreeA(View view)
     {
         scoreA+=3;
+
         displayForTeamA(scoreA);
     }
-    public void buttonForTwo(View view)
+    public void buttonForTwoA(View view)
     {
         scoreA+=2;
         displayForTeamA(scoreA);
     }
-    public void buttonForFreeThrow(View view)
+    public void buttonForFreeThrowA(View view)
     {
         scoreA+=1;
         displayForTeamA(scoreA);
+    }
+    public void buttonForThreeB(View view)
+    {
+        scoreB+=3;
+
+        displayForTeamB(scoreB);
+    }
+    public void buttonForTwoB(View view)
+    {
+        scoreB+=2;
+        displayForTeamB(scoreB);
+    }
+    public void buttonForFreeThrowB(View view)
+    {
+        scoreB+=1;
+        displayForTeamB(scoreB);
+    }
+    public void reset(View view)
+    {
+        scoreA=0;
+        scoreB=0;
+        displayForTeamA(scoreA);
+        displayForTeamB(scoreB);
     }
 }
